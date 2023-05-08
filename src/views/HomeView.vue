ed6360242445ff75b271cf0c1a8a054d973a7597
PeterChuang <peterlife0617@gmail.com>
2023-05-08 16:15:54 +0800

    add get richmenu

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import RichMenuImage from '@/components/RichMenuImage.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <RichMenuImage />
   </main>
 </template>
